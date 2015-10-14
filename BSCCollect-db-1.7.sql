4e91a3ab272dc905d884345ecdc8d2899d92f61c
Hasitha Lakmal <hasithalakmal@users.noreply.github.com>
2015-10-14 09:50:09 +0530

    Test-commit on jira
    remove the sql file comments

--- BSCCollect-db-1.7.sql
+++ BSCCollect-db-1.7.sql
@@ -1,25 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `bsccollect` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `bsccollect`;
--- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
---
--- Host: localhost    Database: bsccollect
--- ------------------------------------------------------
--- Server version	5.5.45-log
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Temporary view structure for view `bscconnect_view`
---
 
 DROP TABLE IF EXISTS `bscconnect_view`;
 /*!50001 DROP VIEW IF EXISTS `bscconnect_view`*/;
