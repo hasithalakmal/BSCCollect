9497f1501066f5235303bae832b9a0bd6ad82741
Hasitha Lakmal <ghasithalakmal@gmail.com>
2015-10-18 22:36:00 +0530

    DB Connection
    batch insertion with commit

--- BSCCollect/SpringHibernateExample/src/main/java/com/bsc/collect/remotedbaccess/ProsedeurControlsRemote.java
+++ BSCCollect/SpringHibernateExample/src/main/java/com/bsc/collect/remotedbaccess/ProsedeurControlsRemote.java
@@ -5,9 +5,12 @@
 package com.bsc.collect.remotedbaccess;
 
 import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,7 +20,7 @@ import java.util.logging.Logger;
  */
 public class ProsedeurControlsRemote {
 
-     private Statement state;
+    private Statement state;
     private ResultSet result;
     DataBaseManagement dbmc;
     Connection con;
@@ -43,22 +46,56 @@ public class ProsedeurControlsRemote {
         return rs;
 
     }
-    
-    public int callQuery(String sql) {
-         int x = 0;
-         try {
-             dbmc = DataBaseManagement.getInstance();
-             con = (Connection) dbmc.setConnetctionRemote();
-             state = con.createStatement();
-             x = state.executeUpdate(sql);
-             
-            // System.out.println(sql);
-             
-         } catch (SQLException ex) {
-             Logger.getLogger(ProsedeurControlsRemote.class.getName()).log(Level.SEVERE, null, ex);
-         }
 
-         return x;
+    public int callQuery(String sql) {
+        int x = 0;
+        try {
+            dbmc = DataBaseManagement.getInstance();
+            con = (Connection) dbmc.setConnetctionRemote();
+            state = con.createStatement();
+            x = state.executeUpdate(sql);
+            System.out.println(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProsedeurControlsRemote.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return x;
+    }
+
+   
+
+    public void insertdatatoremotedb(List metriclist, List datelist, List valuelist) {
+        try {
+            dbmc = DataBaseManagement.getInstance();
+            con = (Connection) dbmc.setConnetctionRemote();
+
+            con.setAutoCommit(false);
+            Statement st = con.createStatement();
+            String INSERT_RECORD = "insert into latestdata(metricname,date,value) values(?,?,?)";
+
+            System.out.println("massooooooooo");
+
+            PreparedStatement pstmt = con.prepareStatement(INSERT_RECORD);
+
+            for (int i = 0; i < metriclist.size(); i++) {
+                pstmt.setString(1, (String) metriclist.get(i));
+                pstmt.setDate(2, (java.sql.Date) datelist.get(i));
+                pstmt.setFloat(3, (Float) valuelist.get(i));
+                System.out.println(metriclist.get(i) + " == " + datelist.get(i) + " == " + valuelist.get(i));
+                pstmt.addBatch();
+            }
+            System.out.println(pstmt);
+            int[] updateCounts = pstmt.executeBatch();
+
+            // since there were no errors, commit
+            con.commit();
+
+          //  rs.close();
+            st.close();
+           // con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProsedeurControlsRemote.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
--- BSCCollect/SpringHibernateExample/src/main/java/com/bsc/collect/remotedbaccess/TrancferData.java
+++ BSCCollect/SpringHibernateExample/src/main/java/com/bsc/collect/remotedbaccess/TrancferData.java
@@ -5,10 +5,14 @@
  */
 package com.bsc.collect.remotedbaccess;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static org.joda.time.format.ISODateTimeFormat.date;
 
 /**
  *
@@ -29,30 +33,41 @@ public class TrancferData {
     
     public void transferdata(){
         try {
+            List metriclist = new ArrayList();
+            List datelist = new ArrayList();
+            List valuelist = new ArrayList();
+            
             ProsedeurControlsRemote pcr = new ProsedeurControlsRemote();
             ProsedeurControlsLocal pcl = new ProsedeurControlsLocal();
             pcr.callQuery("TRUNCATE latestdata;");
             ResultSet res = pcl.callProc("selectConnectData");
-            String sql = "insert into latestdata(metricname,date,value) values";
+          //  String sql = "insert into latestdata(metricname,date,value) values";
             boolean flag=false;
             while(res.next()){
                 String metricname = res.getString(1);
-                String date = res.getString(2);
+                Date date = res.getDate(2);
                 float value = res.getFloat(3);
                 
-                String para= "('"+metricname+"','"+date+"',"+value+")";
+                metriclist.add(metricname);
+                datelist.add(date);
+                valuelist.add(value);
+                
+                
+               // String para= "('"+metricname+"','"+date+"',"+value+")";
                // System.out.println(para);
-                if(flag){
+              /*  if(flag){
                     sql= sql+" , "+para;
                 }else{
                     sql= sql+para;
                     flag = true;
-                }
+                }*/
                
             }
-            sql=sql+";";
-            System.out.println("hello massa... "+ sql);
-            pcr.callQuery(sql);
+            System.out.println(metriclist);
+            pcr.insertdatatoremotedb(metriclist, datelist, valuelist);
+           // sql=sql+";";
+           // System.out.println("hello massa... "+ sql);
+          //  pcr.callQuery(sql);
         } catch (SQLException ex) {
             Logger.getLogger(TrancferData.class.getName()).log(Level.SEVERE, null, ex);
         }
